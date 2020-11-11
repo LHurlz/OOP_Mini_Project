4768e5e03f4b6721876e84217164aef86e876716
Liam <Liam.Hurley2@students.ittralee.ie>
2020-11-11 12:01:22 +0000

    //

---
+++ TestingFiles/LabelPositionTest.java
@@ -0,0 +1,90 @@
+package TestingFiles;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class LabelPositionTest {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("label positioning test");
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = (JPanel) frame.getContentPane();
+        panel.setLayout(null);
+
+        JLabel label=new JLabel("");
+        ImageIcon courtois = new ImageIcon("images/courtois.png");
+        label.setIcon(courtois);
+
+        JButton heightButton = new JButton();
+        heightButton.setBounds(110,240,83,10);
+        heightButton.setOpaque(false);
+        heightButton.setContentAreaFilled(false);
+        heightButton.setBorderPainted(false);
+
+        JButton capsButton = new JButton();
+        capsButton.setBounds(110,256,83,10);
+        capsButton.setOpaque(false);
+        capsButton.setContentAreaFilled(false);
+        capsButton.setBorderPainted(false);
+
+        panel.add(heightButton);
+        panel.add(capsButton);
+        panel.add(label);
+
+        heightButton.addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent e){
+                Color selectedColor = new Color(12,246,214,62);
+                heightButton.setOpaque(true);
+                heightButton.setBackground(selectedColor);
+            }
+
+            public void mouseExited(MouseEvent evt) {
+                heightButton.setOpaque(false);
+                heightButton.setContentAreaFilled(false);
+                heightButton.setBorderPainted(false);
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                JOptionPane.showConfirmDialog(null,"Do you wish to \"call\" the height stat?");
+                heightButton.setOpaque(false);
+                heightButton.setContentAreaFilled(false);
+                heightButton.setBorderPainted(false);
+            }
+        });
+
+        capsButton.addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent e){
+                Color selectedColor = new Color(12,246,214,62);
+                capsButton.setOpaque(true);
+                capsButton.setBackground(selectedColor);
+            }
+
+            public void mouseExited(MouseEvent evt) {
+                capsButton.setOpaque(false);
+                capsButton.setContentAreaFilled(false);
+                capsButton.setBorderPainted(false);
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                JOptionPane.showConfirmDialog(null,"Do you wish to \"call\" the caps stat?");
+                capsButton.setOpaque(false);
+                capsButton.setContentAreaFilled(false);
+                capsButton.setBorderPainted(false);
+            }
+        });
+
+
+        Dimension size = label.getPreferredSize();
+        label.setBounds(0,0,size.width,size.height);
+
+        frame.setSize(225,380);
+        frame.setVisible(true);
+
+    }
+}
--- TestingFiles/MouseListenTest.java
+++ TestingFiles/MouseListenTest.java
@@ -14,9 +14,10 @@ public class MouseListenTest extends JFrame {
         label.setOpaque(true);
         add(label);
 
-        label2 = new JLabel("Defence Label");
+
+        /*label2 = new JLabel("Defence Label");
         label2.setOpaque(true);
-        add(label2);
+        add(label2);*/
 
         label.addMouseListener(new MouseAdapter() {
             public void mouseEntered(MouseEvent evt) {
@@ -40,7 +41,7 @@ public class MouseListenTest extends JFrame {
         setLocationRelativeTo(null);
         setVisible(true);
 
-        label2.addMouseListener(new MouseAdapter() {
+        /*label2.addMouseListener(new MouseAdapter() {
             public void mouseEntered(MouseEvent evt) {
                 Color c = label2.getBackground(); // When the mouse moves over a label, the background color changed.
                 label2.setBackground(label2.getForeground());
@@ -55,7 +56,7 @@ public class MouseListenTest extends JFrame {
             public void mouseClicked(MouseEvent evt){
                 JOptionPane.showConfirmDialog(null,"Are you sure you wish to use the defence stat?");
             }
-        });
+        });*/
     }
     public static void main(String[] args) {
         new MouseListenTest();
